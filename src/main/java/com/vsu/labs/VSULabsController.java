01244db0fb318de979d2713080443876fd575e9d
aloeseva <45678913579@mail.ru>
2022-10-16 15:10:18 +0300

    lab6

--- src/main/java/com/vsu/labs/VSULabsController.java
+++ src/main/java/com/vsu/labs/VSULabsController.java
@@ -2,27 +2,62 @@ package com.vsu.labs;
 
 import com.vsu.labs.lab5.Lab5Service;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class VSULabsController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public Pane paneMain;
-    public Label labelType;
-    public Button buttonCreateFigure;
-    public TextField textFieldSizeOfFigure;
+public class VSULabsController implements Initializable {
+
     public Menu menuLab5;
-    public TextArea textAreaOutputFigure;
+    public Pane paneLab5;
+    public Label labelTypeLab5;
+    public Button buttonCreateFigureLab5;
+    public TextField textFieldSizeOfFigureLab5;
+    public TextArea textAreaOutputFigureLab5;
+    public Menu menuLab6;
+    public Pane paneLab6;
+    public Label labelTypeLab6;
+    public Button buttonCreate1Lab6;
+    public Button buttonCreate2Lab6;
+    public Button buttonCreate3Lab6;
+    public Button buttonCreate4Lab6;
+    public TextArea textAreaOutputFigureLab6;
+    public TextField textField1Lab6;
+    public TextField textField2Lab6;
+    public TextField textField3Lab6;
+    public MenuItem menuItemLab5;
+    public MenuItem menuItemLab6;
+    private Pane selectedPane;
     private double x, y;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        selectedPane = paneLab5;
+        selectedPane.setVisible(true);
+        paneLab6.setVisible(false);
+    }
+
+    @FXML
+    public void menuLab5Click() {
+        selectPane(paneLab5);
+    }
+
+    @FXML
+    public void menuLab6Click() {
+        selectPane(paneLab6);
+    }
 
     @FXML
     protected void buttonCreateFigureOnClick() {
-        int s = inputCheck(textFieldSizeOfFigure.getText());
+        int s = inputCheck(textFieldSizeOfFigureLab5.getText());
         if (s != -1) {
-            textAreaOutputFigure.setText(Lab5Service.figure(s));
+            textAreaOutputFigureLab5.setText(Lab5Service.figure(s));
         }
     }
 
@@ -54,18 +89,39 @@ public class VSULabsController {
     private int inputCheck(String str) {
         int s = -1;
 
-        try{
+        try {
             s = Integer.parseInt(str);
             if (s < 2) {
-                textAreaOutputFigure.setText("Введите число большее или равное 2!");
+                textAreaOutputFigureLab5.setText("Введите число большее или равное 2!");
                 s = -1;
             }
-        }
-        catch (NumberFormatException ex){
-            textAreaOutputFigure.setText("Введите целое число!");
+        } catch (NumberFormatException ex) {
+            textAreaOutputFigureLab5.setText("Введите целое число!");
             ex.printStackTrace();
         }
 
         return s;
+    }
+
+    private void selectPane(Pane pane) {
+        this.selectedPane.setVisible(false);
+        pane.setVisible(true);
+        selectedPane = pane;
+    }
+
+    public void buttonCreate4OnClick() {
+
+    }
+
+    public void buttonCreate3OnClick() {
+
+    }
+
+    public void buttonCreate2OnClick() {
+
+    }
+
+    public void buttonCreate1OnClick() {
+
     }
 }
---
+++ src/main/java/com/vsu/labs/lab6/Lab6Service.java
@@ -0,0 +1,47 @@
+package com.vsu.labs.lab6;
+
+public class Lab6Service {
+    public static String figure(int s) {
+        String str = "";
+        StringBuilder strBuilder = new StringBuilder(str);
+        starDraw(s/2, s/2, true, strBuilder);
+        starDraw(s/2, 1, false, strBuilder);
+        return strBuilder.toString();
+    }
+
+    private static void starDraw(int s, int sIter, boolean inv, StringBuilder str) {
+        if (inv) {
+            str.append(" ".repeat(Math.max(0, s)));
+
+            supplier(s, sIter, str);
+
+            if (sIter != 1) {
+                starDraw(s, sIter-1, true, str);
+            }
+        } else {
+            str.append(" ".repeat(Math.max(0, s - sIter)));
+
+            supplier(s, sIter, str);
+
+            if (sIter < s) {
+                starDraw(s, sIter + 1, false, str);
+            }
+        }
+    }
+
+    private static void supplier(int s, int sIter, StringBuilder str) {
+        if (s == sIter) {
+            str.append("*".repeat(Math.max(0, s)));
+        } else if (sIter > 1) {
+            str.append("*");
+            if (sIter > 2) {
+                str.append("#".repeat(sIter - 2));
+            }
+            str.append("*");
+        } else {
+            str.append("*");
+        }
+        str.append("\n");
+    }
+
+}
